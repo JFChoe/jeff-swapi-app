a1e72bb6f905413633ff8c8f2b778e0b0ee90521
JFChoe <choe.j@husky.neu.edu>
2022-09-04 23:03:36 -0700

    add workign filter list'

--- src/PeopleList.jsx
+++ src/PeopleList.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react";
 import Dropdown from "./Dropdown";
 
 const PeopleList = () => {
+  // would typically store these values in redux once fetched,
+  // but storing in useState hook b/c of time constraint
   const [people, setPeople] = useState([]);
   const [filterValue, setFilterValue] = useState("");
+  const [filteredPeople, setFilteredPeople] = useState([]);
   const [homeworldURLtoHomeworldMapping, setHomeworldURLtoHomeworldMapping] =
     useState(new Map());
   const updateHomeworldMap = (k, v) => {
@@ -41,11 +44,9 @@ const PeopleList = () => {
   }, []);
 
   useEffect(() => {
-    let homeworldList = [];
+    let homeworldList = new Set();
     people?.forEach((person) => {
-      if (!homeworldList.includes(person.homeworld)) {
-        homeworldList.push(person.homeworld);
-      }
+      homeworldList.add(person.homeworld);
     });
     homeworldList.forEach((homeworldUrl) => {
       if (!homeworldURLtoHomeworldMapping.has(homeworldUrl)) {
@@ -59,23 +60,54 @@ const PeopleList = () => {
     homeWorldOptionList.push({ value: value, label: key });
   });
 
-  return (
-    <div>
-      <h2>Person | Home-world are listed below</h2>
+  useEffect(() => {
+    const filteredPeople = [];
+    people.forEach((person) => {
+      if (person.homeworld === filterValue) {
+        filteredPeople.push(person);
+      }
+    });
+    setFilteredPeople(filteredPeople);
+  }, [filterValue]);
+
+  let peopleList;
+
+  if (filterValue) {
+    peopleList = (
       <ul>
-        <Dropdown
-          label="Filter by home-world: "
-          options={homeWorldOptionList}
-          value={filterValue}
-          onChange={(event) => setFilterValue(event.target.value)}
-        />
-        {people?.map((person, i) => (
+        {filteredPeople.map((person, i) => (
           <li key={i}>
             {person.name} |{" "}
             {homeworldURLtoHomeworldMapping.get(person.homeworld)}
           </li>
         ))}
       </ul>
+    );
+  } else if (people.length !== 0) {
+    peopleList = (
+      <ul>
+        {people.map((person, i) => (
+          <li key={i}>
+            {person.name} |{" "}
+            {homeworldURLtoHomeworldMapping.get(person.homeworld)}
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    peopleList = <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>Person | Home-world are listed below</h2>
+      <Dropdown
+        label="Filter by home-world: "
+        options={homeWorldOptionList}
+        value={filterValue}
+        onChange={(event) => setFilterValue(event.target.value)}
+      />
+      {peopleList}
     </div>
   );
 };
